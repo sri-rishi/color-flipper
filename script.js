c43a5f8b7226ca17f237e6d3cf154ff7af94b846
Rishi Srivastava <sririshi2612@gmail.com>
2021-05-25 13:27:34 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let btnClick  =  document.getElementById("btn-click");
+let simpleTypeBtn = document.getElementById("simple-type");
+let hexTypeBtn = document.getElementById("hex-type")
+let colorValue = document.querySelector(".color");
+
+let colorArr = ["Aqua","#4a1c40" , "Blue", "#0a1931", "Fuchsia", "#ffc93c", "Gray", "#ff7b54", "Green", "#fad586", "Lime", "#301b3f" , "Maroon","#810000", "Navy", "#7eca9c", "Olive","#810034", "Purple","#da723c", "Red", "#ebd8b7", "Silver", "Teal", "White", "Yellow"]
+
+
+let hexArr = ["#4a1c40" , "#0a1931", "#ffc93c", "#ff7b54", "#fad586",  "#301b3f", "#810000", "#7eca9c", "#810034", "#da723c", "#ebd8b7"]
+
+
+let called = true;
+
+const turnFalse = () => {
+  called = false;
+}
+
+const turnTrue = () => {
+  called = true;
+}
+
+const clickHandler = () => {
+  let randomIndex = Math.floor(Math.random() * colorArr.length);
+
+  let randomColor = colorArr[randomIndex];
+  // console.log(randomColor);
+
+  let randomHexIndex = Math.floor(Math.random() * hexArr.length);
+
+  let randomHex = hexArr[randomHexIndex];
+  document.body.style.backgroundColor = called === true ? randomColor : randomHex;
+  colorValue.innerHTML = called === true ? randomColor : randomHex;
+}
+
+
+btnClick.addEventListener("click", clickHandler);
+simpleTypeBtn.addEventListener("click", turnTrue);
+hexTypeBtn.addEventListener("click", turnFalse);
